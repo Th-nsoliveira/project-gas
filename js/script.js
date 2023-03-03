873f82a08a34c36958bcd189f7d54e3e9fc29d1c
Thiago Nascimento <122623611+Th-nsoliveira@users.noreply.github.com>
2023-03-03 19:27:48 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,29 @@
+function preco(){
+    var etanol=document.querySelector('#etanol').value;
+    var gasolina=document.querySelector('#gasolina').value;
+    var resultado=document.querySelector('.resultado');
+    
+
+    if(etanol!='' && gasolina!=''){
+
+        resultado.classList.add('show-result')
+
+        if(etanol<gasolina*0.7){
+            resultado.innerHTML='<h3>Abasteça com : <br>'+ '<span> Etanol</span>';
+            
+        }
+        else{
+            resultado.innerHTML='<h3>Abasteça com : <br>'+ '<span> Gasolina</span>';
+        }
+
+    }
+    else{
+        alert('Insira os dados')
+    }
+}
+
+var btn=document.querySelector('#btn');
+
+btn.addEventListener('click', function(){
+    preco();
+})
